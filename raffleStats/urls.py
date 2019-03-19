03516c0cd57193e1f82b6a117693b97f5fd7e63d
Josh Pedowitz <jpedowitz@middlebury.edu>
2019-03-18 23:45:56 -0400

    adding raffle detail page

--- raffleStats/urls.py
+++ raffleStats/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
+from django.conf.urls import url
 
 from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    url(r'^(?P<vpost_id>[0-9a-zA-Z]{6})', views.details, name='details'),
 ]
--- raffleStats/views.py
+++ raffleStats/views.py
@@ -1,11 +1,16 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
-from .models import Raffle
+from .models import Raffle, SpotCount
 
 def index(request):
     raffles = Raffle.objects.order_by('-datetime_completed')
     context = {'raffles': raffles}
     return render(request, 'raffleStats/index.html', context)
 
+def details(request, vpost_id):
+    spot_counts = SpotCount.objects.filter(post_id = vpost_id).order_by('num_spots')
+    raffle = Raffle.objects.get(post_id = vpost_id)
+    context = {'spot_counts': spot_counts, 'raffle':raffle}
+    return render(request, 'raffleStats/details.html', context)
 # Create your views here.
