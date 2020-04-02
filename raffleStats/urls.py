6cb94497a971b43c5bca19f05ded83266128256e
Josh Pedowitz <jpedowitz@middlebury.edu>
2020-04-02 16:30:55 -0400

    Infinite scroll and cleanup.
    Added infinite scroll on the raffleStats index page using waypoints and 
    JQuery. Cleaned up home to show its under construction

--- raffleStats/urls.py
+++ raffleStats/urls.py
@@ -4,7 +4,8 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    #path('', views.index, name='index'),
+    path('', views.raffle_list, name="index"),
     url(r'^(?P<vpost_id>[0-9a-zA-Z]{6})/$', views.details, name='details'),
     url(r'^api/spotcounts/(?P<vpost_id>[0-9a-zA-Z]{6})/$', views.spotcounts, name='spotcounts'),
 ]
--- raffleStats/views.py
+++ raffleStats/views.py
@@ -2,11 +2,30 @@ from django.shortcuts import render
 from django.http import HttpResponse, JsonResponse
 from django.template import loader
 from .models import Raffle, SpotCount
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
-def index(request):
-    raffles = Raffle.objects.order_by('-datetime_completed')
+'''class RaffleHistoryView(ListView):
+    model = Raffle
+    paginate_by = 10
+    context_object_name = 'raffles'
+    template_name = 'index.html'
+    ordering = ['datetime_completed']'''
+
+def raffle_list(request):
+    raffles = []
+    raffles_full = Raffle.objects.order_by('-datetime_completed')
+    page = request.GET.get('page', 1)
+    paginator = Paginator(raffles_full, 20)
+
+    try:
+        raffles = paginator.page(page)
+    except PageNotAnInteger:
+        raffles = paginator.page(1)
+    except EmptyPage:
+        raffles = paginator.page(paginator.num_pages)
+
     context = {'raffles': raffles}
-    return render(request, 'raffleStats/index.html', context)
+    return render(request, 'raffleStats/raffle_list.html', context)
 
 def details(request, vpost_id):
     spot_counts = SpotCount.objects.filter(post_id = vpost_id).order_by('num_spots')
